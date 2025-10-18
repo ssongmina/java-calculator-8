133993d607fdcf9f4064f37daa0854d4b5a86020
ssongmina <thdalsdk5498@naver.com>
2025-10-18 18:10:05 +0900

    feat: 문자열 파싱 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,9 +1,14 @@
 package calculator.controller;
 
+import calculator.model.Calculator;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
+import java.util.ArrayList;
+
 public class CalculatorController {
+
+    private Calculator calculator;
 
     public void powerOnCalculator(){
         // 입력
@@ -18,11 +23,14 @@ public class CalculatorController {
 
     public int calculate(String input){
         // 입력값 검증
+        calculator = new Calculator();
         validate(input);
+
         // 문자열 파싱
+        ArrayList<Integer> numbers = parseString(input);
 
         // 결과값 계산
-
+        //return addNum(numbers);
         return 0;
     }
 
@@ -34,20 +42,19 @@ public class CalculatorController {
         // 커스텀 구분자의 선언이 틀린 경우
         // 첫글자가 숫자도, 기본 구분자도 아니면 -> 커스텀 구분자를 선언한거야.
         char c = input.charAt(0);
-        char delimiter = ' ';
         if(!(('0' <= c && c <= '9') || (c == ',') || (c == ':'))){
             if(!input.startsWith("//")) throw new IllegalArgumentException("커스텀 구분자의 선언이 틀렸습니다");
             if(!input.substring(3,5).equals("\\n")) throw new IllegalArgumentException("커스텀 구분자의 선언이 틀렸습니다");
             char custom = input.charAt(2);
             if('0' <= custom && custom <= '9') throw new IllegalArgumentException("커스컴 구분자의 선언이 틀렸습니다");
-            delimiter = custom;
+            calculator.setDelimiter(custom);
         }
         // 커스텀, 기본 구분자 이외의 특수기호가 존재하는 경우
         for(int i=5; i<input.length();i++){
             c = input.charAt(i);
             if('0' <= c && c <= '9') continue;
             else if(c == ':' || c == ',') continue;
-            else if(delimiter != ' ' && c == delimiter) continue;
+            else if(calculator.isDelimiter() && c == calculator.getDelimiter()) continue;
             else throw new IllegalArgumentException("커스텀, 기본 구분자 이외의 특수기호가 존재합니다");
         }
 
@@ -56,4 +63,31 @@ public class CalculatorController {
             throw new IllegalArgumentException("음수가 존재합니다");
         }
     }
+
+    public ArrayList<Integer> parseString(String input){
+        // 커스텀 구분자가 있는지 확인
+        boolean flag = calculator.isDelimiter();
+        if(flag){
+            input = input.substring(5);
+        }
+        // 커스텀, 기본 구분자 기준으로 파싱하기
+        ArrayList<Integer> numbers = new ArrayList<>();
+        String str = "";
+        for(int i=0; i<input.length(); i++){
+            // 구분자면 패스 -> string reset
+            char c = input.charAt(i);
+            if(c == ':' || c == ',' || (flag && c == calculator.getDelimiter())){
+                numbers.add(Integer.parseInt(str));
+                str = "";
+            }
+            // 구분자가 아니면 string +=
+            else{
+                str += String.valueOf(c);
+            }
+        }
+        if(str != "") numbers.add(Integer.parseInt(str));
+        return numbers;
+    }
+
+
 }
